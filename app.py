9c68ac1d18c11ee698c890571df4f4adc85ab45a
antoniocallie97-web <antoniocallie97@gmail.com>
2026-07-10 13:14:49 +0300

    changes on app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_migrate import Migrate
+
 from config import Config
 from models import db
 from resources.auth import Register, Login
@@ -15,11 +16,34 @@ migrate = Migrate(app, db)
 
 api = Api(app)
 
+
+# Home Route
+@app.route("/")
+def home():
+    return {
+        "message": "Welcome to the Productivity App API",
+        "status": "running",
+        "version": "1.0",
+        "available_endpoints": {
+            "Register": "POST /register",
+            "Login": "POST /login",
+            "Get Notes": "GET /notes?page=1&per_page=5",
+            "Create Note": "POST /notes",
+            "Get Single Note": "GET /notes/<id>",
+            "Update Note": "PATCH /notes/<id>",
+            "Delete Note": "DELETE /notes/<id>"
+        }
+    }, 200
+
+
+# Authentication Routes
 api.add_resource(Register, "/register")
 api.add_resource(Login, "/login")
 
+# Notes Routes
 api.add_resource(NoteListResource, "/notes")
 api.add_resource(NoteResource, "/notes/<int:id>")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
